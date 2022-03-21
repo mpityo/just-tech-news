8b98f7463c0c5a7cf00eb03f980903543812332c
Matt Pityo <mpityo@gmail.com>
2022-03-20 22:32:29 -0400

    single post and create post api routes

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -20,4 +20,45 @@ router.get('/', (req, res) => {
     });
 });
 
+// get single post
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'post_url', 'title', 'created_at'],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// create post
+router.post('/', (req, res) => {
+    Post.create({
+        title: req.body.title,
+        post_url: req.body.post_url,
+        user_id: req.body.user_id
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
