359d0edaf23000c22d6cd0b976261d3098600e6a
Matt Pityo <mpityo@gmail.com>
2022-03-21 20:14:41 -0400

    introduce vote model

--- models/index.js
+++ models/index.js
@@ -1,3 +1,16 @@
 const User = require('./User');
+const Vote = require('./Vote');
+
+User.belongsToMany(Post, {
+	through: Vote,
+	as: 'voted_posts',
+	foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+	through: Vote,
+	as: 'voted_posts',
+	foreignKey: 'post_id'
+});
 
 module.exports = { User };
