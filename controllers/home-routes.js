ac4fc39fb85dd4a4a539956e1c8f0045906cc76e
Matt Pityo <mpityo@gmail.com>
2022-03-23 20:36:59 -0400

    spell check and file refactor from merging

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const sequalize = require('../config/connection');
+const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.engine('handlebars', 'hbs.engine');
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, 'public')));
