8e9d2ae93e1727c325dec74b9982d10c25a0bf96
Matt Pityo <mpityo@gmail.com>
2022-03-27 11:50:31 -0400

    add partials and accompaying tests

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 // create session (cookies)
 const sess = {
 	secret: 'Super secret secret',
